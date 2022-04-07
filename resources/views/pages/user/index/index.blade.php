bea22ae7f09ace96ca1562f60634547ebcfa47c4
asim7056 <asim@einnovention.co.uk>
2022-04-07 09:27:54 +0500

    1q

--- resources/views/pages/user/index/index.blade.php
+++ resources/views/pages/user/index/index.blade.php
@@ -7035,14 +7035,14 @@
                                                 <input type="text" class="form-control" placeholder=".. الايميل هنا"
                                                     readonly="" value="asimali7056@gmail.com">
                                             </div>
-                                            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mt-4">
+                                            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mt-5">
                                                 <label class="form-label">شكل الطباعة</label>
                                                 <input type="text" class="form-control" placeholder=".. التخصص هنا"
                                                     readonly="" value="رقم الهاتف هنا">
                                             </div>
                                             <div class="col-sm-12 col-md-6 col-lg-1 col-xl-1 col-xxl-1 hide-tab"></div>
                                             <div
-                                                class="col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mt-4 quantity-m-50">
+                                                class="col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mt-5 quantity-m-50">
                                                 <label class="form-label">تقنية الطباعة</label>
                                                 <input type="text" class="form-control" placeholder=".. الجنس هنا"
                                                     readonly="" value="male">
