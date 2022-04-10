6e5ecf20838d3de63119c2b6b9e03e9ba338cc95
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2022-04-11 00:25:30 +0500

    login, dynamic pdf, register

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,8 +34,13 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (\Exception $e) {
+            if ($e->getPrevious() instanceof \Illuminate\Session\TokenMismatchException) {
+                return redirect()->route('login');
+            };
         });
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
     }
 }
