0b4d480ef55caabbc391edf50ef09756caa38e80
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2022-04-12 13:15:47 +0500

    image upload

--- app/Http/Controllers/PerposalController.php
+++ app/Http/Controllers/PerposalController.php
@@ -44,7 +44,8 @@ class PerposalController extends Controller
      */
     public function store(Request $request)
     {
-        $image = $request->image->getClientOriginalName();
+        $img = $request->file('image');
+        $image = $img->getClientOriginalName();
         $request->image->move(public_path('upload/'), $image);
         $pathimage ='upload/'.$image;
         $med = Medical::find($request->order_id);
