56f4aa00a86f6eaaebf7c0136e84181d70357709
asim7056 <asim@einnovention.co.uk>
2022-04-13 17:45:53 +0500

    ar

--- resources/views/livewire/chats.blade.php
+++ resources/views/livewire/chats.blade.php
@@ -40,17 +40,18 @@
                         <form wire:submit.prevent="SendMessage">
                             <div class="row">
                                 <div class="col-md-9">
-                                    <input wire:model="message" style="pointer-events: unset" class="form-control input shadow-none w-100 d-inline-block" id="message" placeholder="Type a message" >
+                                    <input wire:model="message" style="pointer-events: unset" class="form-control input shadow-none w-100 d-inline-block" id="message" placeholder="اكتب رسالة
+" >
                                 </div>
                              
                                 <div class="col-md-3">
                                     <!-- <button type="submit" class="btn btn-primary d-inline-block w-100"><i class="far fa-paper-plane"></i> Send</button> -->
-                                    <button type="submit" class="btn btn-form chat-submit-btn"><i class="far fa-paper-plane"></i> Send</button>
+                                    <button type="submit" class="btn btn-form btn-primary chat-submit-btn"><i class="fa fa-paper-plane"></i> يرسل</button>
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <!-- <input type="file" wire:model="photo" id="photo-{{ $i }}"> -->
-                                <button type="button" class="btn btn-sky chat-img-upload mt-4">
+                                <button type="button" class="btn btn-sky btn-primary chat-img-upload mt-4">
                                     دقت قلب          
                                     <img src="{{ asset('/user/assets/icons/arrow-right.svg')}}" alt="arrow-right">
                                     <input type="file" wire:model="photo" id="photo-{{ $i }}">
