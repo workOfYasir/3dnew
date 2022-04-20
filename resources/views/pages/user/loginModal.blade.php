d388699c7fc0ac721e956e1fd770ad8dc2e0fa5e
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2022-04-20 18:27:53 +0500

    debugged

--- resources/views/pages/user/loginModal.blade.php
+++ resources/views/pages/user/loginModal.blade.php
@@ -37,9 +37,7 @@
                                         <div class="tab-pane fade show active" id="userloginModal" role="tabpanel">
                                             <form action="{{route('login')}}" class="row gy-4 gx-5" method="post">
                                                 @csrf
-                                                <input type="hidden" name="role" value="admin">
-                                                <input type="hidden" name="roleUser" value="user">
-                                                <input type="hidden" name="roleAdmin" value="admin">
+                                                <input type="hidden" name="role" value="12">
                                                 <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
                                                     <label class="form-label dot">الايميل</label>
                                                     <input type="email" name="email" class="form-control email" placeholder="الايميل هنا .." value="">
@@ -65,7 +63,7 @@
                                         <div class="tab-pane fade designerModel" id="designerloginModal" role="tabpanel" aria-labelledby="profile-tab">
                                             <form action="{{route('login')}}" class="row gy-4 gx-5" method="post">
                                                 @csrf
-                                                <input type="hidden" name="role" value="designer">
+                                                <input type="hidden" name="role" value="3">
                                                 <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
                                                     <label class="form-label dot">الايميل</label>
                                                     <input type="email" name="email" class="form-control email" placeholder="الايميل هنا .." value="">
