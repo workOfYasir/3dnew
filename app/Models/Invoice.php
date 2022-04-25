37b8cd22aa05252a86e90b24fbf1a4551dcd6602
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2022-04-25 21:22:42 +0500

    payment and comments

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -17,4 +17,9 @@ class Invoice extends Model
         return $this->belongsTo(User::class);
         
     }
+    public function payment(){
+
+        return $this->hasOne(Payment::class);
+        
+    }
 }
