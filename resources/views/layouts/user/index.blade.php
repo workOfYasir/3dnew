a82abb531cc40eaca087361907f7a358b90d2a26
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2022-04-25 19:27:44 +0500

    signature api

--- resources/views/layouts/user/index.blade.php
+++ resources/views/layouts/user/index.blade.php
@@ -43,7 +43,7 @@
     <script src="{{asset('js/perposalNotAccepted.js')}}"></script>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.signature.js') }}"></script>
 
     <script>
         var perposalAccepted = "{{ route('perposalAccept',':id') }}";
