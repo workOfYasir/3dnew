8b3296a2da5f40946aa0d2d7d95e1bc45f0e9241
AhsanRoomi <ahsan@einnovention.co.uk>
2022-04-30 01:13:31 -0700

    nn
    nn

--- resources/views/pages/user/index/index.blade.php
+++ resources/views/pages/user/index/index.blade.php
@@ -543,7 +543,8 @@
             {{-- <div class="move-blue2"></div> --}}
             <div class="move-blue2"></div>
             <div class="black-bg2"></div>
-            <img src="{{ asset('user/assets/images/hands.png') }}" class="img-fluid hands sample move-hand" alt="hands" data-aos="fade-down">
+            <img src="{{ asset('user/assets/images/hands.png') }}" class="img-fluid hands sample move-hand" alt="hands" data-aos="fade-down"
+            data-aos-duration="1500" data-aos-delay="3000">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-12">
